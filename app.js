548f2df71172ca228e4f8d34b1594d09fc903bb2
Jasdeep Singh <j.singhhh917@gmail.com>
2025-02-17 16:01:25 +1100

    getComputed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const allLis = document.querySelectorAll('li');
+const colours = ['red','orange','yellow','green','blue','purple'];
 
 // printing the content of the Li's
 // for (let i = 0; i < allLis.length; i++) {
@@ -11,6 +12,12 @@ const allLis = document.querySelectorAll('li');
 // }
 
 // changing the html
-for (let li of allLis) {
-  li.innerHTML = 'Pep Guardiola is the <b> GOAT! </b>'
-}
+// for (let li of allLis) {
+//   li.innerHTML = 'Pep Guardiola is the <b> GOAT! </b>'
+// }
+
+// changing the colour of the Li's
+// allLis.forEach((li, i) => {
+//   const colour = colours[i];
+//   li.style.color = colour;
+// })
